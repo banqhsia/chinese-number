bb4e13206d84fcb86fed61544de6bbcac599bc99
Benyi <banqhsia@msn.com>
2019-05-06 23:49:21 +0800

    create CurrencyTest covering currency decorator

--- tests/Unit/AssertionHelper.php
+++ tests/Unit/AssertionHelper.php
@@ -6,11 +6,11 @@ trait AssertionHelper
 {
     private function givenNumber($number)
     {
-        $this->actual = (string) $this->target->number($number);
+        $this->target = $this->target->number($number);
     }
 
     private function resultShouldBe($expected)
     {
-        $this->assertEquals($expected, $this->actual);
+        $this->assertEquals($expected, (string) $this->target);
     }
 }
---
+++ tests/Unit/Decorator/CurrencyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Decorator;
+
+use PHPUnit\Framework\TestCase;
+use Tests\Unit\AssertionHelper;
+use banqhsia\ChineseNumber\ChineseNumber;
+
+class CurrencyTest extends TestCase
+{
+    use AssertionHelper;
+
+    /**
+     * @var ChineseNumber
+     */
+    private $target;
+
+    public function setUp()
+    {
+        $this->target = new ChineseNumber;
+    }
+
+    /**
+     * @testWith ["tw", "150", "新臺幣一百五十元"]
+     *           ["tw", "150000", "新臺幣十五萬元"]
+     *           ["tw", "150000000", "新臺幣一億五千萬元"]
+     *
+     *           ["hk", "150", "港幣一百五十元"]
+     *           ["hk", "150000", "港幣十五萬元"]
+     *           ["hk", "150000000", "港幣一億五千萬元"]
+     *
+     *           ["cn", "150", "人民币一百五十元"]
+     *           ["cn", "150000", "人民币十五万元"]
+     *           ["cn", "150000000", "人民币一亿五千万元"]
+     */
+    public function test_should_get_localed_currency($locale, $given, $expected)
+    {
+        $this->givenNumber($given);
+
+        $this->target->setLocale($locale);
+        $this->target->currency();
+
+        $this->resultShouldBe($expected);
+    }
+}
