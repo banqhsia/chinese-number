b9b9c87002936e9d88f7ca45f27a58b6ddbebabc
Benyi <banqhsia@msn.com>
2019-05-06 17:57:55 +0800

    Refactor PositiveNumberTest using @testWith annotation

--- tests/Unit/PositiveNumberTest.php
+++ tests/Unit/PositiveNumberTest.php
@@ -23,112 +23,29 @@ class PositiveNumberTest extends TestCase
         $this->assertInstanceOf(ChineseNumber::class, $actual);
     }
 
-    public function test_105_should_get_一百○五()
+    /**
+     * @testWith [105, "一百○五"]
+     *           [200, "二百"]
+     *           [-200, "負二百"]
+     *           [2050, "二千○五十"]
+     *           [2056, "二千○五十六"]
+     *           [2500, "二千五百"]
+     *           [10000, "一萬"]
+     *           [10005, "一萬○五"]
+     *           [10090, "一萬○九十"]
+     *           [10900, "一萬○九百"]
+     *           [19000, "一萬九千"]
+     *           [19005, "一萬九千○五"]
+     *           [19050, "一萬九千○五十"]
+     *           [19500, "一萬九千五百"]
+     *           [19503, "一萬九千五百○三"]
+     *           [19530, "一萬九千五百三十"]
+     *           [19532, "一萬九千五百三十二"]
+     */
+    public function test_positive_number($given, $expected)
     {
-        $this->givenNumber(105);
-        $this->resultShouldBe('一百○五');
-    }
-
-    public function test_200_should_get_二百()
-    {
-        $this->givenNumber(200);
-        $this->resultShouldBe('二百');
-    }
-
-    public function test_minus_200_should_get_負二百()
-    {
-        $this->givenNumber(-200);
-        $this->resultShouldBe('負二百');
-    }
-
-    public function test_2050_should_get_二千○五十()
-    {
-        $this->givenNumber(2050);
-        $this->resultShouldBe('二千○五十');
-    }
-
-    public function test_2056_should_get_二千○五十六()
-    {
-        $this->givenNumber(2056);
-        $this->resultShouldBe('二千○五十六');
-    }
-
-    public function test_2500_should_get_二千五百()
-    {
-        $this->givenNumber(2500);
-        $this->resultShouldBe('二千五百');
-    }
-
-    public function test_10000_should_get_一萬()
-    {
-        $this->givenNumber(10000);
-        $this->resultShouldBe('一萬');
-    }
-
-    public function test_10005_should_get_一萬零五()
-    {
-        $this->givenNumber(10005);
-        $this->resultShouldBe('一萬○五');
-    }
-
-    public function test_10090_should_get_一萬零九十()
-    {
-        $this->givenNumber(10090);
-        $this->resultShouldBe('一萬○九十');
-    }
-
-    public function test_10900_should_get_一萬零九百()
-    {
-        $this->givenNumber(10900);
-        $this->resultShouldBe('一萬○九百');
-    }
-
-    public function test_19000_should_get_一萬九千()
-    {
-        $this->givenNumber(19000);
-        $this->resultShouldBe('一萬九千');
-    }
-
-    public function test_19005_should_get_一萬九千○五()
-    {
-        $this->givenNumber(19005);
-        $this->resultShouldBe('一萬九千○五');
-    }
-
-    public function test_19050_should_get_一萬九千○五十()
-    {
-        $this->givenNumber(19050);
-        $this->resultShouldBe('一萬九千○五十');
-    }
-
-    public function test_19500_should_get_一萬九千五百()
-    {
-        $this->givenNumber(19500);
-        $this->resultShouldBe('一萬九千五百');
-    }
-
-    public function test_19503_should_get_一萬九千五百○三()
-    {
-        $this->givenNumber(19503);
-        $this->resultShouldBe('一萬九千五百○三');
-    }
-
-    public function test_19530_should_get_一萬九千五十三()
-    {
-        $this->givenNumber(19530);
-        $this->resultShouldBe('一萬九千五百三十');
-    }
-
-    public function test_19532_should_get_一萬九千五十三二()
-    {
-        $this->givenNumber(19532);
-        $this->resultShouldBe('一萬九千五百三十二');
-    }
-
-    public function tesrDown()
-    {
-        unset($this->target);
-        unset($this->actual);
+        $this->givenNumber($given);
+        $this->resultShouldBe($expected);
     }
 
     private function givenNumber($number)
